270b4747aa1435b90393f1992bc06beda780ed8c
fThAbhishek-Pandey <abhishek830564@email.com>
2024-10-15 00:53:33 +0530

    routing usecontext react-toastify axios setup and foalder structure completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState,useContext, useEffect } from 'react'
+import { AdminContext } from './Context/AdminContext'
+import { CoAdminContext } from './Context/CoAdmin'
+import Login from './pages/Login'
+import AdminHome from './pages/Admin/AdminHome'
+import HomeCoAdmin from './pages/CoAdmin/HomeCoAdmin'
+import { ToastContainer, toast } from 'react-toastify';
 function App() {
-  const [count, setCount] = useState(0)
-
+const {admin_token} = useContext( AdminContext)
+const {coAdmin_token} = useContext(CoAdminContext);
+const notify = () => toast("Wow so easy!");
+useEffect (()=>{
+  notify ()
+},[])
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {
+          !(admin_token || coAdmin_token ) 
+          ? <Login />
+          : <div>
+            {admin_token && <AdminHome /> }
+            {coAdmin_token&& <HomeCoAdmin /> }
+          </div>
+
+      }
+       <ToastContainer />
     </>
   )
 }
