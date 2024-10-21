57683bcc6810b1fd91b705102b2f0bd53c48e7b7
fThAbhishek-Pandey <abhishek830564@email.com>
2024-10-21 23:43:56 +0530

    login page added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,27 +1,17 @@
-import React, { useState } from "react";
-
+import React, { useState , useContext} from "react";
+import { AdminContext } from "../Context/AdminContext";
 const Login = () => {
-  const [loggedIn, setLoggedIn] = useState(false);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-
+  const {handleLoginAdmin } = useContext(AdminContext);
   const handleLogin = (e) => {
     e.preventDefault();
-    if (username && password) {
-      setLoggedIn(true);
-    }
+    handleLoginAdmin(username,password);
   };
-
-  const handleLogout = () => {
-    setLoggedIn(false);
-    setUsername("");
-    setPassword("");
-  };
-
   return (
     <div className="flex h-screen justify-center items-center bg-gray-100">
       <div className="border p-8 bg-white rounded-md shadow-lg">
-        {!loggedIn ? (
+        {(
           <form onSubmit={handleLogin} className="space-y-4">
             <h2 className="text-2xl font-bold text-center">Admin Panel</h2>
             <div>
@@ -51,16 +41,6 @@ const Login = () => {
               Login
             </button>
           </form>
-        ) : (
-          <div className="text-center">
-            <h2 className="text-2xl font-bold">Welcome, {username}!</h2>
-            <button
-              onClick={handleLogout}
-              className="mt-4 bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600 transition duration-300"
-            >
-              Logout
-            </button>
-          </div>
         )}
       </div>
     </div>
