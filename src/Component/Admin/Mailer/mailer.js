989c93ea276c3a6314c99a6cd02a89fa64b24b04
fThAbhishek-Pandey <abhishek830564@email.com>
2024-12-05 03:14:40 +0530

    demo

--- src/Component/Admin/Mailer/mailer.js
+++ src/Component/Admin/Mailer/mailer.js
@@ -3,13 +3,14 @@ import { toast } from "react-toastify"
 const Mailer = async (backendURL, adminToken,sheet_id,employee) => {
   console.log("Mailer --> ",backendURL,adminToken,employee)
    try {
-    const {data} = await axios.post(backendURL+'/admin/mail/one',{sheet_id,employee}, {headers:{admintoken:adminToken}});
-    if(data.success){
-      toast.success(data.message);
-    }
-    else {
-     toast.error(data.message)
-    }
+    toast.success("this is Demo");
+    // const {data} = await axios.post(backendURL+'/admin/mail/one',{sheet_id,employee}, {headers:{admintoken:adminToken}});
+    // if(data.success){
+    //   toast.success(data.message);
+    // }
+    // else {
+    //  toast.error(data.message)
+    // }
    } catch (error) {
     console.log(error);
     toast.error(error.message);
